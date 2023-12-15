a201af02831cc4e94334d04fbc99ae60d606eb3c
Monica Rotulo <monica.rotulo@gmail.com>
2023-12-15 10:48:40 +0100

    config now supports multiple datafiles

--- OpenSTL/configs/kmc/VTUNet.py
+++ OpenSTL/configs/kmc/VTUNet.py
@@ -1,7 +1,11 @@
 method = "VTUNet"
 
 # kmc data
-datafile = "kmc/exp_1_complete_2D.h5"
+datafile = [
+    ("exp_1_complete_2D.h5", 90),
+    ("exp_1_complete_2D.h5", 45),
+    ("exp_3_complete_2D.h5", 90),
+]
 num_frames_per_experiment = 90
 in_shape = [12, 1, 128, 128]
 img_size = (12, 100, 100)
@@ -14,16 +18,15 @@ total_length = 24
 patch_size = (4, 4, 4)
 in_chans = 1
 embed_dim = 96
-depths = [2, 2, 2, 1]  # num layers
-# depths_decoder = [1, 2, 2, 2]
+depths = [2, 2, 2, 1]
 num_heads = [3, 6, 12, 24]
-window_size = (2, 7, 7)
+window_size = (7, 7, 7)
 zero_head = False
 num_classes = 1  # output_channels
 model_type = "gSTA"
 
 # training
-lr = 1e-3
-batch_size = 16
+lr = 1e-4
+batch_size = 128
 drop_path = 0
 sched = "onecycle"
