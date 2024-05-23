86bc835bd06503f6d80193e51a9ead2cb0c44778
Monica Rotulo <monica.rotulo@gmail.com>
2024-05-23 16:44:27 +0200

    update train test scripts

--- tools/test.py
+++ tools/test.py
@@ -1,20 +1,35 @@
 # Copyright (c) CAIRI AI Lab. All rights reserved
 
 import warnings
-warnings.filterwarnings('ignore')
+import yaml
+
+warnings.filterwarnings("ignore")
 
 from openstl.api import BaseExperiment
-from openstl.utils import (create_parser, default_parser, get_dist_info, load_config,
-                           setup_multi_processes, update_config)
+from openstl.utils import (
+    create_parser,
+    default_parser,
+    get_dist_info,
+    load_config,
+    setup_multi_processes,
+    update_config,
+)
 
 try:
     import nni
+
     has_nni = True
-except ImportError: 
+except ImportError:
     has_nni = False
 
 
-if __name__ == '__main__':
+def load_yaml_config(yaml_file, yaml_name="default"):
+    with open(yaml_file, "r") as file:
+        all_configs = yaml.safe_load(file)
+    return all_configs.get(yaml_name, all_configs["default"])
+
+
+if __name__ == "__main__":
     args = create_parser().parse_args()
     config = args.__dict__
 
@@ -23,18 +38,31 @@ if __name__ == '__main__':
         config.update(tuner_params)
 
     assert args.config_file is not None, "Config file is required for testing"
-    config = update_config(config, load_config(args.config_file),
-                           exclude_keys=['method', 'batch_size', 'val_batch_size'])
+    config = update_config(
+        config,
+        load_config(args.config_file),
+        exclude_keys=["method", "batch_size", "val_batch_size"],
+    )
+
+    yfile = "../configs/kmc/experiments.yaml"  # config["yaml_file"]
+    yaml_name = args.ex_name if args.ex_name else "default"
+    yaml_config = load_yaml_config(yfile, yaml_name)
+
+    config = update_config(
+        config,
+        yaml_config,
+    )
+
     default_values = default_parser()
     for attribute in default_values.keys():
         if config[attribute] is None:
             config[attribute] = default_values[attribute]
-    config['test'] = True
+    config["test"] = True
 
     # set multi-process settings
     setup_multi_processes(config)
 
-    print('>'*35 + ' testing  ' + '<'*35)
+    print(">" * 35 + " testing  " + "<" * 35)
     exp = BaseExperiment(args)
     rank, _ = get_dist_info()
 
--- tools/train.py
+++ tools/train.py
@@ -2,6 +2,8 @@
 
 import os.path as osp
 import warnings
+import yaml
+import wandb
 
 warnings.filterwarnings("ignore")
 
@@ -15,6 +17,13 @@ from openstl.utils import (
     update_config,
 )
 
+
+def load_yaml_config(yaml_file, yaml_name="default"):
+    with open(yaml_file, "r") as file:
+        all_configs = yaml.safe_load(file)
+    return all_configs.get(yaml_name, all_configs["default"])
+
+
 try:
     import nni
 
@@ -27,12 +36,14 @@ if __name__ == "__main__":
     args = create_parser().parse_args()
     config = args.__dict__
 
+    print(args.method)
+
     if has_nni:
         tuner_params = nni.get_next_parameter()
         config.update(tuner_params)
 
     cfg_path = (
-        osp.join("./configs", args.dataname, f"{args.method}.py")
+        osp.join("../configs", args.dataname, f"{args.method}.py")
         if args.config_file is None
         else args.config_file
     )
@@ -44,13 +55,25 @@ if __name__ == "__main__":
             config,
             loaded_cfg,
             exclude_keys=[
-                "method",
-                "batch_size",
-                "val_batch_size",
+                # "method",
+                # "batch_size",
+                # "val_batch_size",
                 "drop_path",
                 "warmup_epoch",
             ],
         )
+        print(args.method)
+        # update values from experiment yaml:
+        yfile = "../configs/kmc/experiments.yaml"  # config["yaml_file"]
+        yaml_name = args.ex_name if args.ex_name else "default"
+        yaml_config = load_yaml_config(yfile, yaml_name)
+
+        config = update_config(
+            config,
+            yaml_config,
+        )
+
+        # load default values
         default_values = default_parser()
         for attribute in default_values.keys():
             if config[attribute] is None:
@@ -59,8 +82,14 @@ if __name__ == "__main__":
     # set multi-process settings
     setup_multi_processes(config)
 
+    wandb.init(
+        project=f"Diagnostic",  # {config['method']} Reg_VTUNet openSTL_PF Diagnostic
+        entity="mrotulo",
+        sync_tensorboard=True,
+    )
+
     print(">" * 35 + " training " + "<" * 35)
-    args.method = "VTUNet"
+
     exp = BaseExperiment(args)
     rank, _ = get_dist_info()
     exp.train()
